fe21db1885061cc097ccd1e683d95e372e96124f
Spiritusanti <mccrackenjacob@gmail.com>
2022-09-26 11:27:38 -0700

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assests");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   return {
     dir: {
