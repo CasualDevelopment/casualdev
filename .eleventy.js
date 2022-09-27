d56f434ffb02cb5dc0d1ecdaba9cd4bac1434aa2
Spiritusanti <mccrackenjacob@gmail.com>
2022-09-27 09:05:57 -0700

    styled blog snippet page

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assests");
   eleventyConfig.addPassthroughCopy("./src/admin");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
